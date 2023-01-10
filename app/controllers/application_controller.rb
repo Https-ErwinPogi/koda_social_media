ab0c54cd3892b0a2f4b6fd1bb013ea73ff1d72c2
KodaCamp <>
2023-01-10 14:56:46 +0800

    Fix code - add order desc in comment and post index - add before authenticate action to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   def after_sign_up_path_for(resource)
     after_sign_in_path_for(resource) if is_navigational_format?
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.includes(:user)
+    @posts = Post.includes(:user).order(id: :desc)
   end
 
   def new
